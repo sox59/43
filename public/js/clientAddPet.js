8e5f8bb6c69407a4bfa9580dce80f93b5a979fd1
Paul <ppagliughi@yahoo.com>
2024-12-26 04:31:15 -0600

    17th commit

--- public/js/clientAddPet.js
+++ public/js/clientAddPet.js
@@ -1,6 +1,16 @@
 document.querySelector("#add-new-pet-form").addEventListener("submit", function (e) {
   e.preventDefault()
 
+  const pet = {
+
+    name: document.querySelector("#name").value,
+    birthYear: document.querySelector("#birthYear").value,
+    species: document.querySelector("#species").value,
+    description: document.querySelector("#description").description
+
+  }
+
+  console.log(pet)
 
 
 })
